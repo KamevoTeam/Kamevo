470eb1bd3938f80d373bc1b2ec1da3fba20797f0
krazzbeluh <paulleclerc@laposte.net>
2016-09-20 22:02:31 +0200

    insertion de mon script d'inscription dans le projet.

---
+++ YTBH-final/envoi.php
@@ -0,0 +1,8 @@
+<?php
+	$req = $bdd1->prepare('INSERT INTO `users` (`ID`,  `pseudo`, `password`, `Nom`, `mail`, `grade`) VALUES (NULL, :pseudo, :password, :nom, :mail, 1)');
+	$req->execute(array(
+		'pseudo' => $pseudo,
+		'password' => $pass,
+		'nom' => $nom,
+		'mail' => $mail));
+?>
--- inscription/traitement.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$nom = htmlspecialchars($_POST['nom']);
-	$pseudo = htmlspecialchars($_POST['pseudo']);
-	$pass = md5(sha1(htmlspecialchars($_POST['pass'])));
-	$confirm = md5(sha1(htmlspecialchars($_POST['confirm'])));
-	$mail = htmlspecialchars($_POST['mail']);
-?>
