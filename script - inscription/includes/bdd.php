d072f805a9ffb2cd7b626006c29e38be6a45f2ae
krazzbeluh <paulleclerc@laposte.net>
2016-09-20 21:58:41 +0200

    Un peu de ménage

--- script - inscription/includes/bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=UTF8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-} catch (Exception $e) {
-	die('Erreur : ' . $e->getMessage());
-}
-?>
--- script - inscription/inscription/traitement.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$nom = htmlspecialchars($_POST['nom']);
-	$prenom = htmlspecialchars($_POST['prenom']);
-	$pseudo = htmlspecialchars($_POST['pseudo']);
-	$pass = md5(sha1(htmlspecialchars($_POST['pass'])));
-	$confirm = md5(sha1(htmlspecialchars($_POST['confirm'])));
-	$mail = htmlspecialchars($_POST['mail']);
-?>
