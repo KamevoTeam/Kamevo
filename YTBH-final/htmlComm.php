a537b91240119c813f6c7befed5fe1112dcbec14
Paul Leclerc <paulleclerc@laposte.net>
2016-09-21 20:26:39 +0200

    ajout d'une base pourl les commentaires

---
+++ YTBH-final/htmlComm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset='UTF-8' />
+</head>
+
+<body>
+	<form action='.' method='POST'>
+		<input type='textarea' name='comment' id='comment' />
+		<input type='submit' />
+	</form>
+</body>
+</html>
