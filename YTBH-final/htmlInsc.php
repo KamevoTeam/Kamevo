341aecd0cc264a86ddccb976841912d9c585641d
krazzbeluh <paulleclerc@laposte.net>
2016-09-21 21:29:45 +0200

    no message

--- YTBH-final/htmlInsc.php
+++ YTBH-final/htmlInsc.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset='UTF-8' />
-	<script src='../../jquery-3.1.0.min.js'></script>
+	<script src='jquery-3.1.0.min.js'></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 </head>
