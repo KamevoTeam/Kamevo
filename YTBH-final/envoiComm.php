0e1bbb7e50b7f030fdba452ceab038b09cb32db7
Paul Leclerc <paulleclerc@laposte.net>
2016-10-02 16:08:53 +0200

    pas grand chose

--- YTBH-final/envoiComm.php
+++ YTBH-final/envoiComm.php
@@ -1,7 +1,7 @@
 <?php
 	if (isset($_SESSION['ID'])) {
 		if (!empty($_SESSION['ID'])) {
-			$req = $bdd->prepare('INSERT INTO `comments` (`id_post`, `poster`, `comment`) VALUES (`:ID`, `:user`, `:comment`)');
+			$req = $bdd->prepare('INSERT INTO `comments` (`id_post`, `poster`, `comment`, `date`) VALUES (`:ID`, `:user`, `:comment`, NULL)');
 			$req->execute(array(
 				'ID' => $_GET['id'],
 				'user' => $_SESSION['ID'],
