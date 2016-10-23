00b5515a4d64fc87c2fdf40a50c1ff798aa58418
krazzbeluh <paulleclerc@laposte.net>
2016-10-23 15:31:16 +0200

    fusion des scripts commentaires et htmlcomm

--- YTBH-final/commentaires.php
+++ YTBH-final/commentaires.php
@@ -9,6 +9,39 @@ include('php/commentaires.php');
 			sendComm($_SESSION['ID'], $_POST['commentaire'], $_GET['ID']);
 		}
 	}
+?>
 
-	include('htmlComm.php');
+
+<div class='block'>
+		
+		<?php
+		if (isset($_SESSION['ID'])) { if (!empty($_SESSION['ID'])) {
+				$okForTextarea = true;?>
+			<form action='posts.php?id=5' method='POST' style='text-align: center;'>
+			<textarea name='commentaire' id='commentaire' placeholder='Commentez' rows='5' cols='70'></textarea>
+			<input type='submit' />
+			</form>
+		<?php } else {
+			$okForTextarea = false;
+		}}else {
+			$okForTextarea = false;
+		}
+		if ($okForTextarea == false) {
+			echo '<h4 style="text-align: center;">Vous devez vous <a href=\'connexion.php?redirect=posts&id=5\'>connecter</a> pour pouvoir envoyer des commentaires !</h4>';
+		}
+		
+				$order = 'default';
+			if (isset($_GET['order'])) {
+				if (!empty($_GET['order'])) {
+					if ($_GET['order'] == 'time') {
+						$order = 'time';
+					}
+				}
+			}
+		if (isset($_GET['id'])) {if (!empty($_GET['id'])){
+			getComm(30, $order, $_GET['id']);
+		}}
+		
+		
+include('footer.php');
 ?>
