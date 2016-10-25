74d5b6000b7deeb9400475b3545ff7b67e0cba7d
Zelkyo <axel@skyfight.be>
2016-10-25 22:59:17 +0300

    Par Axel | Ajout de la page d'accueil

--- kamevo_src/DESIGN/menu.php
+++ kamevo_src/DESIGN/menu.php
@@ -4,6 +4,9 @@
 	    <a href="#" class="log" id="icon"></a>
 	     <a href="#"><li class="logo"> K </li></a><div class="line-separator"></div>
 	    <a href="#"><li class="title"> KAMEVO</li></a>
+	    <div class="mme">
+	         <a class="js" href="#"><li class="link">Connexion</li></a>
+	     </div>
 	  </ul>
 	 </nav>
 	</header>
