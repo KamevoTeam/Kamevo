8b9729b9d76e4e897b7b93025285c116afc88aad
Zelkyo <axel@skyfight.be>
2016-10-25 22:46:34 +0300

    Par Axel | Ajout de la page d'accueil du site

---
+++ kamevo_src/DESIGN/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kamevo - Bienvenue</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.max.css">
+	<link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+</head>
+<body>
+<?php require("menu.php") ?>
+	<div class="container-left">
+		<img src="coding.png" width="100%" alt='logo' class="wannado">
+	</div>
+	<div class="container-right">
+	<h1 class="main-title">Bienvenue sur Kamevo.com</h1>
+	<h4>Pour continuer votre navigation , veuillez vous créer un compte :</h4><br />
+      <form class="content">
+      <div class="form-row">
+        <input type="text" class="input-text" id="input" placeholder="Pseudo" />
+        <label class="label-helper" for="input">Pseudo</label>
+      </div>
+
+      <div class="form-row" >
+        <input type="text" class="input-text" id="input2" placeholder="Prénom" />
+        <label class="label-helper" for="input2">Prénom</label>
+      </div>
+
+      <div class="form-row" >
+        <input type="password" class="input-text" id="input3" placeholder="Mot de passe" />
+        <label class="label-helper" for="input3">Mot de passe</label>
+      </div>
+
+      <div class="form-row" >
+        <input type="password" class="input-text" id="input4" placeholder="Confirmation" />
+        <label class="label-helper" for="input3">Confirmation de votre Mot de passe</label>
+      </div>
+
+      <div class="form-row" >
+        <input type="email" class="input-text" id="input5" placeholder="Adresse E-mail" />
+        <label class="label-helper" for="input3">Adresse E-Mail</label>
+      </div>
+
+      <div class="form-row" >
+        <input type="email" class="input-text" id="input6" placeholder="Mot de passe" />
+        <label class="label-helper" for="input3">Confirmation de votre Adresse E-Mail</label>
+      </div>
+      <input type="submit" name="submit" class="leto" value="S'inscire">
+      </form> <!-- end .content -->
+	</div>
+</body>
+</html>
---
+++ kamevo_src/DESIGN/menu.php
@@ -0,0 +1,9 @@
+	<header class="header">
+	 <nav class="menu" id="top">
+	  <ul>
+	    <a href="#" class="log" id="icon"></a>
+	     <a href="#"><li class="logo"> K </li></a><div class="line-separator"></div>
+	    <a href="#"><li class="title"> KAMEVO</li></a>
+	  </ul>
+	 </nav>
+	</header>
