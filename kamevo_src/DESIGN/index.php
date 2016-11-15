c911097216a06a833dc932add66dc677469b38a8
Zelkyo <axel@skyfight.be>
2016-11-15 20:04:03 +0200

    AXEL | Ajout de la connexion + date de naissance
    AXEL | Ajout de la connexion + date de naissance

--- kamevo_src/DESIGN/index.php
+++ kamevo_src/DESIGN/index.php
@@ -3,7 +3,6 @@
 <head>
 	<title>Kamevo - Bienvenue</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="style.max.css">
 	<link rel="stylesheet" href="menu.css">
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
@@ -13,46 +12,38 @@
 <?php require("menu.php") ?>
 	<div class="container-left">
 		<img src="coding.png" width="100%" alt='logo' class="wannado">
-	</div>
-	<div class="container-right">
+	</div><div class="container-right">
 	<h1 class="main-title">Bienvenue sur Kamevo.com</h1>
 	<h4>Pour continuer votre navigation , veuillez vous créer un compte :</h4><br />
       <form class="content">
       <div class="form-row">
         <input type="text" class="input-text" id="input" placeholder="Pseudo" />
-        <label class="label-helper" for="input">Pseudo</label>
-      </div>
-
-      <div class="form-row" >
-        <input type="text" class="input-text" id="input2" placeholder="Prénom" />
+         <label class="label-helper" for="input">Pseudo</label>
+        </div><div class="form-row" >
+         <input type="text" class="input-text" id="input2" placeholder="Prénom" />
         <label class="label-helper" for="input2">Prénom</label>
-      </div>
-
-      <div class="form-row" >
-        <input type="password" class="input-text" id="input3" placeholder="Mot de passe" />
+      </div><div class="form-row" >
+        <input type="date" class="input-text" id="input-date" value="2000-06-08" />
+         <label class="label-helper" for="input-date">Date de naissance</label>
+        </div><div class="form-row" >
+         <input type="password" class="input-text" id="input3" placeholder="Mot de passe" />
         <label class="label-helper" for="input3">Mot de passe</label>
-      </div>
-
-      <div class="form-row" >
-        <input type="password" class="input-text" id="input4" placeholder="Confirmation" />
+      </div><div class="form-row" >
+       <input type="password" class="input-text" id="input4" placeholder="Confirmation" />
         <label class="label-helper" for="input3">Confirmation de votre Mot de passe</label>
-      </div>
-
-      <div class="form-row" >
-        <input type="email" class="input-text" id="input5" placeholder="Adresse E-mail" />
+        </div><div class="form-row" >
+         <input type="email" class="input-text" id="input5" placeholder="Adresse E-mail" />
         <label class="label-helper" for="input3">Adresse E-Mail</label>
-      </div>
-
-      <div class="form-row" >
-        <input type="email" class="input-text" id="input6" placeholder="Mot de passe" />
+      </div><div class="form-row" >
+         <input type="email" class="input-text" id="input6" placeholder="Confirmation" />
         <label class="label-helper" for="input3">Confirmation de votre Adresse E-Mail</label>
       </div>
-      <input type="submit" name="submit" class="leto" value="S'inscire">
+       <input type="submit" name="submit" class="leto" value="S'inscire">
       <div class="checkall">
       	<input type="checkbox" name="checked" id="checks" class="checks">
         J'ai lu et j'accepte les <a href="#">conditions d'utilisations</a> de Kamevo.com .
       </div>
-      </form> <!-- end .content -->
+      </form> <!-- end of the content -->
 	</div>
 </body>
 </html>
--- kamevo_src/DESIGN/menu.php
+++ kamevo_src/DESIGN/menu.php
@@ -1,12 +1,12 @@
 	<header class="header">
-	 <nav class="menu" id="top">
-	  <ul>
+	 <nav class="menu" id="top"><ul>
 	    <a href="#" class="log" id="icon"></a>
 	     <a href="#"><li class="logo"> K </li></a><div class="line-separator"></div>
 	    <a href="#"><li class="title"> KAMEVO</li></a>
 	    <div class="mme">
-	         <a class="js" href="#"><li class="link">Connexion</li></a>
-	     </div>
-	  </ul>
-	 </nav>
-	</header>
+	    <form class="connect" method="post" acion="">
+	      <input type="email" name="connect-email" class="connect-text" placeholder="Adresse E-mail" />
+	    	<input type="password" name="connect-password" class="connect-text-bis" placeholder="Mot de passe" />
+	       <input type="submit" class="connect-submit" value=" Se connecter" />
+	    </form></div></ul>
+	 </nav></header>
