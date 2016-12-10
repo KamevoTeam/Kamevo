108cdaf102ce8eb330a6fde0bad984c8391edc04
Zelkyo <axel@skyfight.be>
2016-12-10 19:56:33 +0200

    Ajout de la page de détails d'une publication
    Page visibe après avoir cliqué sur "En savoir plus >" dans la publication du fil d'actualité

---
+++ kamevo_src/DESIGN/details/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Publication de William</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="css/line-separator.css">
+	<link rel="stylesheet" href="css/block.css">
+	<link rel="stylesheet" href="css/style.max.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+</head>
+<body>
+	<?php require("menu.php");
+	require("addons.php");
+	require("pub.php") ?>
+	<div class="container">
+	<?php 
+		require("block.php");
+	?>	
+	</div>
+</body>
+</html>
