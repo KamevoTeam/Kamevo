489ff844e88b45e9b00062c9c69f82dbd2cd349e
Zelkyo <axel@skyfight.be>
2016-12-10 17:26:14 +0200

    Page de profil | Axel - 10 décembre
    Page de profil | Axel - 10 décembre

---
+++ kamevo_src/DESIGN/user/php/profile.php
@@ -0,0 +1,5 @@
+ <div class="profile-header">
+  <h3 class="name">WILLIAM | Wistaro</h3>
+   <img class="profile-img" src="../img/team.png" alt="profile-image">
+  <img class="cover-img" src="../img/banner.png" alt="cover-image">
+ </div>
