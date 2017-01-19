b58610cd2467fb658be50612a0e1102d62d39c98
DESKTOP-7O9I1H3\Axel <axel@skyfight.be>
2017-01-19 20:34:32 +0100

    La popup est fonctionelle sur la page de post

--- kamevo_src/post/index.php
+++ kamevo_src/post/index.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="../frameworks/w3.css">
     <link rel="stylesheet" href="../frameworks/font-awesome/css/font-awesome.min.css">
-    <script type="text/javascript" src="frameworks/jquery.min.js"></script>
+    <script type="text/javascript" src="../frameworks/jquery.min.js"></script>
 </head>
 <body>
 	<?php require("../menu_co.php"); ?>
