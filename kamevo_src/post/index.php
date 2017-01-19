7c5a58d23f3d3238bb90698012228c8f9c5ca6de
DESKTOP-7O9I1H3\Axel <axel@skyfight.be>
2017-01-19 20:32:54 +0100

    Débuggage du design de la page de post ...

--- kamevo_src/post/index.php
+++ kamevo_src/post/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php session_start(); ?>
 <html>
 <head>
@@ -19,24 +18,4 @@
 	</div>
 <script src="../js/explore.js"></script>
 </body>
-=======
-<?php session_start(); ?>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Kamevo - Publier</title>
-	<link rel="stylesheet" href="css/style.max.css">
-	<link rel="stylesheet" href="css/post.css">
-	<link rel="stylesheet" href="css/menu.css">
-    <link rel="stylesheet" href="../frameworks/w3.css">
-    <link rel="stylesheet" href="../frameworks/font-awesome/css/font-awesome.min.css">
-    <script type="text/javascript" src="frameworks/jquery.min.js"></script>
-</head>
-<body>
-	<?php require("php/menu.php"); ?>
-	<div class="container">
-		<?php require("php/post.php"); ?>
-	</div>
-</body>
->>>>>>> 643795f9215336b97ba1b23be0b52ecee084b770
 </html>
