d695e9e5ebd1a79303dddd87bdf43d5ebd64cba0
DESKTOP-7O9I1H3\Axel <axel@skyfight.be>
2017-01-31 17:15:15 +0100

    Ajout de la page de settings

---
+++ kamevo_src/settings/index.php
@@ -0,0 +1,22 @@
+
+<?php session_start(); ?>
+<html>
+<head>
+ <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/menu_co.css">
+   <link rel="stylesheet" href="../css/popup.css">
+  <link rel="stylesheet" href="../frameworks/w3.css">
+   <link rel="stylesheet" href="css/style.max.css">
+  <link rel="stylesheet" href="css/settings.css">
+   <link rel="stylesheet" href="../frameworks/font-awesome/css/font-awesome.min.css">
+  <script type="text/javascript" src="../frameworks/jquery.min.js"></script>
+ <title>Kamevo - Paramètres</title>
+</head>
+<body>
+ <?php require("../menu_co.php"); ?>
+  <div class="container">
+   <?php require("php/settings.php"); ?>
+  </div>
+  <script src="../js/explore.js"></script>
+</body>
+</html>
---
+++ kamevo_src/settings/php/settings.php
@@ -0,0 +1,33 @@
+<div class="settings">
+	<div class="header">
+		<h3 class="header-tilte">Modifier mon profil :</h3>
+		<div class="header-separation"></div>
+	</div>
+	 <div class="informations">
+ 	  <form action="" class="infos" method="post">
+ 	  <h5 class="info-label">Nom : </h5>
+ 	   <input type="text" value="Zelkyo" id="info-input" class="inline input-info">
+ 	  <h5 class="info-label">Nom : </h5>
+ 	   <input type="text" value="Zelkyo" id="info-input" class="inline input-info">
+ 	  </form>
+ 	 </div>
+	<div class="basics">
+ 	  <h5 class="file-label">Modifier ma photo de profil : </h5>
+ 	   <input class="inline input-file" id="my-file" type="file" name="profile">
+      <label for="my-file" class="input-file-trigger" tabindex="0">Choisir un fichier ...</label>
+       <br/><h5 class="file-label">Modifier ma photo de couverture : </h5>
+ 	    <input class="inline input-file" id="my-file" type="file" name="cover">
+      <label for="my-file" class="input-file-trigger" tabindex="0">Choisir un fichier ...</label>
+ 	 </div>
+    <div class="text">
+    <h3 class="header-tilte">Ajouter une biographie à mon profil :</h3>
+		<div class="header-separation"></div>
+     <form action="" method="post">
+  	  <textarea name="text" class="text-area" placeholder="Votre message ..."></textarea>
+  	  *Votre biographie ne peut contenir que 80 caractères maximum
+     </form>
+    </div>
+   <form action="" id="php" method="post">
+  	<input type="submit" class="submit" name="submit" value="Enregistrer">
+  </form>
+</div>
