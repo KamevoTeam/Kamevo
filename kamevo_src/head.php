266cf96ff95f7463de27c989ffe20521b5ccd02b
DESKTOP-7O9I1H3\Axel <axel@skyfight.be>
2017-02-05 18:43:05 +0100

    Résolution de quelques bugs d'affichage

--- kamevo_src/head.php
+++ kamevo_src/head.php
@@ -1,25 +1,25 @@
-	<title>YTBH - Accueil</title>
+	<title>Kamevo - Accueil</title>
 	<meta charset="utf-8">
 	<!--  ORGINAL INCLUSIONS  !-->
 
 	<link rel="stylesheet" href="css/feed.css">
 	<link rel="stylesheet" href="css/block.css">
-	<link rel="stylesheet" href="css/pub.css">
+	<link rel="stylesheet" href="css/pub.css"> 
 	<link rel="stylesheet" href="css/social.css">
 	<link rel="stylesheet" href="css/line-separator.css">
 	<link rel="stylesheet" href="css/style.max.css">
 	<link rel="stylesheet" href="css/popup.css">
 	<link rel="stylesheet" href="css/menu_co.css">
-    <link rel="stylesheet" href="frameworks/w3.css">
-    
+  <link rel="stylesheet" href="css/likes.css">
+
+
     <!--  LOADING FRAMWORKS   !-->
     <link rel="stylesheet" href="frameworks/font-awesome/css/font-awesome.min.css">
     <script type="text/javascript" src="frameworks/jquery.min.js"></script>
 
     <!-- LINKED CSS FROM DETAILS TO LOAD !-->
-    <link rel="stylesheet" href="DESIGN/details/css/progressBar.css">
-    <link rel="stylesheet" href="css/likes-details.css">
-
+    <link rel="stylesheet" href="css/likes.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 
     <!-- LOADING JAVASCRIPT/AJAX  !-->
     <script type="text/javascript" src="js/createHttpObject.js"></script>
@@ -45,5 +45,3 @@
 		}));
 
     </script>
-
-
