8d48eabb95924a6fc356fb82208630bf7c725506
krazzbeluh <paulleclerc@laposte.net>
2017-02-24 16:24:59 +0100

    ajout de la vraie possibilité de se co avec l'email et début du mail d'inscription

---
+++ kamevo_src/php/mailInsc.php
@@ -0,0 +1,5 @@
+<?php
+$subject = 'Kamevo - Inscription';
+$message = 'Vous avez effectué une requête d\'inscription sur le site Kamevo.com, afin de finaliser cette inscription, vous devez cliquer sur ce lien\r\n
+Si vous n\'avez pas demandé cette inscription, veuillez ignorer ce mail';
+?>
