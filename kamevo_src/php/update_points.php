6fe3c271a68e593ba29f3b446e88f82a3d8fa7ca
ROMIGUIERES William <wistaro@hotmail.fr>
2017-06-04 19:04:34 +0200

    Ajout du fichier de script (vide pour le moment, à remplir)

---
+++ kamevo_src/php/update_points.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* This script calculate points with up-to-date datas for each users, and update field 'points' on table 'users'. 
+*
+* It's run every first of month, by a cron task
+*/
+	
+	require('co_pdo.php');
+
+?>
