ed1d77db6fc08a844aff07b9eaf8aa7801d17004
ROMIGUIERES William <wistaro@hotmail.fr>
2017-09-24 16:12:08 +0200

    Delete some useless files

--- kamevo_src/post-examples.php
+++
@@ -1,9 +0,0 @@
-<div class="block">
-  	<div class="block-title">
-	 	<h6 class="block-name"><strong>Aucun post</strong></h6>
-	 </div>
-	<p class="block-bio">qsldkj n'a pas encore posté de message! <br /></p>
-</div>
-
-
-
